7d49b28ddeb4bab23f39bf0a52c46f3e03320cba
Sean Jonker <sean.jonker@kurtosys.com>
2020-10-29 12:31:50 +0200

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,47 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 import { Elephant } from './classification/species/elephant';
+import { Lion } from './classification/species/lion';
+import { Leopard } from './classification/species/leopard';
 
 class App extends React.Component<any, any> {
+	activeAnimal!: Elephant | Lion | Leopard;
+	buffalo!: any;
+	elephant!: Elephant;
+	leopard!: Leopard;
+	lion!: Lion;
+	rhino!: any;
+
 	constructor(props: any) {
 		super(props);
 		this.selectAnimal = this.selectAnimal.bind(this);
+		this.elephant = new Elephant();
+		this.leopard = new Leopard();
+		this.lion = new Lion();
 	}
-
-	activeAnimal: any;
 
 	selectAnimal(evt: React.ChangeEvent<HTMLSelectElement>) {
 		const selectedValue = evt.target.value;
 
-		if (selectedValue === 'elephant') {
-			this.activeAnimal = new Elephant();
-			console.log(this.activeAnimal.makeSound());
+		switch (selectedValue) {
+			case 'buffalo':
+				break;
+			case 'elephant':
+				this.activeAnimal = this.elephant;
+				break;
+			case 'leopard':
+				this.activeAnimal = this.leopard;
+				break;
+			case 'lion':
+				this.activeAnimal = this.lion;
+				break;
+			case 'rhino':
+				break;
+			default:
+				break;
 		}
+
+		console.log(this.activeAnimal.makeSound());
 	}
 
 	render() {
@@ -29,6 +54,8 @@ class App extends React.Component<any, any> {
 						<select onChange={this.selectAnimal}>
 							<option value="">none</option>
 							<option value="elephant">Elephant</option>
+							<option value="lion">Lion</option>
+							<option value="leopard">Leopard</option>
 						</select>
 					</div>
 				</header>
